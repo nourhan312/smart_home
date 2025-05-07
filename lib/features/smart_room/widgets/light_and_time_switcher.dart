6a3dec2aee7b894e1e004f703fd420b966eef799
Nour <nourhanaymanghonim@gmail.com>
2025-05-08 01:50:24 +0300

    ir sensor

--- lib/features/smart_room/widgets/light_and_time_switcher.dart
+++ lib/features/smart_room/widgets/light_and_time_switcher.dart
@@ -17,11 +17,66 @@ class _LightsAndTimerSwitchersState extends State<LightsAndTimerSwitchers> {
   bool _isLight1On = false;
   bool _isLight2On = false;
   int _lightValue = 0;
+  int _irSensorValue = 1;
+  bool _isWarningVisible = false;
+  OverlayEntry? _warningOverlay;
 
   @override
   void initState() {
     super.initState();
     setupFirebaseListener();
+  }
+
+  @override
+  void dispose() {
+    _removeWarning();
+    super.dispose();
+  }
+
+  void _showWarning() {
+    if (_isWarningVisible) return;
+
+    _isWarningVisible = true;
+    _warningOverlay = OverlayEntry(
+      builder:
+          (context) => Positioned(
+            top: MediaQuery.of(context).padding.top + 20,
+            left: 20,
+            right: 20,
+            child: Material(
+              color: Colors.transparent,
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.red[400],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Row(
+                  children: [
+                    Icon(Icons.warning_amber_rounded, color: Colors.white),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Text(
+                        'Warning! Object detected nearby!',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+    );
+
+    Overlay.of(context).insert(_warningOverlay!);
+  }
+
+  void _removeWarning() {
+    if (!_isWarningVisible) return;
+
+    _warningOverlay?.remove();
+    _warningOverlay = null;
+    _isWarningVisible = false;
   }
 
   void setupFirebaseListener() {
@@ -30,7 +85,6 @@ class _LightsAndTimerSwitchersState extends State<LightsAndTimerSwitchers> {
       if (data != null) {
         setState(() {
           _lightValue = int.parse(data.toString());
-          // _isLightOn = _lightValue > 0;
         });
       }
     });
@@ -50,6 +104,22 @@ class _LightsAndTimerSwitchersState extends State<LightsAndTimerSwitchers> {
         setState(() {
           _isLight2On = data == 1;
         });
+      }
+    });
+
+    _databaseRef.child('irSensor').onValue.listen((event) {
+      final data = event.snapshot.value;
+      if (data != null) {
+        final newValue = int.parse(data.toString());
+        setState(() {
+          _irSensorValue = newValue;
+        });
+
+        if (newValue == 0) {
+          _showWarning();
+        } else {
+          _removeWarning();
+        }
       }
     });
   }
